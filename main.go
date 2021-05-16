ffaeb74b3df4667cdc1bb14d9af6b499a8d56589
lucas de souza <souzaxx@lucass-MacBook-Pro.local>
2021-05-16 16:04:44 -0300

    Added a content-type header

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>Welcome to my awesome sire!</h1>")
+}
+
+func main() {
+	http.HandleFunc("/", handlerFunc)
+	http.ListenAndServe(":3000", nil)
+}
